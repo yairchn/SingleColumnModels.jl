287c000fc7bbf246f8bc857d88c69147b05c5a0c
yairchn <yairchn@caltech.edu>
2020-04-13 23:17:37 -0700

    styarting to add TRMM_LBA

--- src/EDMF/cases.jl
+++ src/EDMF/cases.jl
@@ -2,6 +2,7 @@
 
 export Case
 export BOMEX
+export TRMM_LBA
 
 """
     Case
@@ -23,4 +24,18 @@ Reference:
   Meteorological Society 50.6 (1969): 394-403.
 """
 struct BOMEX <: Case end
+
+"""
+    BOMEX
+
+Continental deep convection case based on the Large scale
+Biosphere-Atmosphere experiment with data from the Tropical
+Rain Measurement (TRMM-LBA).
+
+Reference:
+  Grabowski, et al. (2006). Daytime convective development over land:
+  A model683intercomparison based on lba observations.
+  Quarterly Journal of the Royal684Meteorological Society,132(615), 317–344.
+"""
+struct TRMM_LBA <: Case end
 
